4d29c093803ddc1fae0c7df39cd8a4ce0d8fb1d7
AlejandroBalladares <44703962+AlejandroBalladares@users.noreply.github.com>
2021-03-05 19:41:22 -0300

    Cambio en la logica de movimiento

--- src/main/java/modelo/tablero/Posicion.java
+++ src/main/java/modelo/tablero/Posicion.java
@@ -5,7 +5,6 @@ import modelo.excepciones.PosicionFueraDeRangoException;
 public class Posicion {
     private final int posX;
     private final int posY;
-    protected static final int DISTANCIA_DE_MOVIMIENTO = 1;
 
     public Posicion(int posX, int posY, Tablero tablero) {
         this.sonCoordenadasValidas(posX, posY, tablero);
@@ -21,33 +20,20 @@ public class Posicion {
         }
     }
 
-    public Posicion moverDerecha(Tablero tablero) {
-        return new Posicion(this.posX + DISTANCIA_DE_MOVIMIENTO, this.posY, tablero );
+    public Posicion moverDerecha(Tablero tablero, int distancia) {
+        return new Posicion(this.posX + distancia, this.posY, tablero );
     }
 
-    public Posicion moverIzquierda(Tablero tablero) {
-        return new Posicion(this.posX - DISTANCIA_DE_MOVIMIENTO, this.posY, tablero );
+    public Posicion moverIzquierda(Tablero tablero, int distancia) {
+        return new Posicion(this.posX - distancia, this.posY, tablero );
     }
 
-    public Posicion moverArriba(Tablero tablero) {
-        return new Posicion(this.posX, this.posY + DISTANCIA_DE_MOVIMIENTO, tablero );
+    public Posicion moverArriba(Tablero tablero, int distancia) {
+        return new Posicion(this.posX, this.posY + distancia, tablero );
     }
-    public Posicion moverAbajo(Tablero tablero) {
-        return new Posicion(this.posX, this.posY - DISTANCIA_DE_MOVIMIENTO, tablero );
+    public Posicion moverAbajo(Tablero tablero, int distancia) {
+        return new Posicion(this.posX, this.posY - distancia, tablero );
     }
-
-/*public void moverAbajo() {
-        this.posY = this.posY-DISTANCIA_DE_MOVIMIENTO;
-    }
-    public void moverArriba() {
-        this.posY = this.posY+DISTANCIA_DE_MOVIMIENTO;
-    }
-    public void moverIzquierda() {
-        this.posX = this.posX-DISTANCIA_DE_MOVIMIENTO;
-    }
-    public void moverDerecha() {
-        this.posX = this.posX+DISTANCIA_DE_MOVIMIENTO;
-    }*/
 
     public int getPosX(){
         return this.posX;
