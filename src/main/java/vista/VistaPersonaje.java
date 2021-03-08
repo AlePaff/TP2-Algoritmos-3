5ffe26c936c5aeae4395d88beeef035b8fc6e5e5
lauradefeo <ldefeo@fi.uba.ar>
2021-03-07 23:38:30 -0300

    imagen en el avatar

--- src/main/java/vista/VistaPersonaje.java
+++ src/main/java/vista/VistaPersonaje.java
@@ -3,6 +3,7 @@ package vista;
 import modelo.AlgoBlocks;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.paint.Color;
+import javafx.scene.image.Image;
 
 public class VistaPersonaje {
 
@@ -10,7 +11,7 @@ public class VistaPersonaje {
     Canvas canvas;
     double ancho;
     double alto;
-    int RADIO = 10;
+    int RADIO = 30;
 
     public VistaPersonaje(AlgoBlocks algoBlocks, Canvas canvasCentral) {
         this.algoBlocks = algoBlocks;
@@ -22,8 +23,10 @@ public class VistaPersonaje {
 
     public void dibujar(){
         this.clean();
-        canvas.getGraphicsContext2D().setFill(Color.BLUE);
-        canvas.getGraphicsContext2D().fillOval(algoBlocks.getPosicionDelPersonaje().getPosX() * 12 , algoBlocks.getPosicionDelPersonaje().getPosY() * 12, RADIO, RADIO);
+        Image image = new Image("file:src/main/java/vista/imagenes/avatar.jpg",RADIO,RADIO,false,false);
+        //canvas.getGraphicsContext2D().setFill(Color.BLUE);
+        canvas.getGraphicsContext2D().drawImage(image,algoBlocks.getPosicionDelPersonaje().getPosX() * 12,algoBlocks.getPosicionDelPersonaje().getPosY() * 12 );
+        //canvas.getGraphicsContext2D().fillOval(algoBlocks.getPosicionDelPersonaje().getPosX() * 12 , algoBlocks.getPosicionDelPersonaje().getPosY() * 12, RADIO, RADIO);
 
     }
 
